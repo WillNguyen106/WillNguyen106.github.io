c6c69106b2b09dd8777db7fd9ee24d622ba5e7ee
Will Nguyen <qnguye33@uncc.edu>
2022-02-20 21:31:50 -0500

    updated

---
+++ itis3135/scripts/secondjs.js
@@ -0,0 +1,33 @@
+// Declare global variable and global array
+var numberOfSide;
+const array = ["henagon","digon","trigon","tetragon","pentagon","hexagon","heptagon","octagon","enneagon"];
+
+
+// Create function that asking user to input there number from 1 to 10
+function userInput()
+{
+    numberOfSide = parseInt(prompt("The Walnut Numbat would like your number: "))
+    validateEntry();
+    document.getElementById("polygon").innerHTML = 'This is a ' + getShape() + '.\n' + 'Have a good day!';
+}
+
+// Create a validation function to make sure the entry is a valid number
+function validateEntry()
+{
+    while(numberOfSide > 10 || isNaN(numberOfSide))
+    {
+        alert("Your number is not valid. Please enter your number again(1-10)");
+        numberOfSide = parseInt(prompt("The Walnut Numbat would like your number: "))
+
+    }
+
+}
+
+// Create a function that return a polygon name
+function getShape()
+{
+    var shape;
+    shape  = array[numberOfSide - 1];
+    return shape;
+
+}
