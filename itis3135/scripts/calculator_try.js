f486b76714242c8a1f2a504e22ed6eace2bd37b5
Will Nguyen <qnguye33@uncc.edu>
2022-02-24 16:28:04 -0500

    updated

---
+++ itis3135/scripts/calculator_try.js
@@ -0,0 +1,25 @@
+// Declare variable for calculator
+var number1;
+var number2;
+
+window.onload = function()
+{
+    document.getElementById("button1").onclick = showNum;
+    document.getElementById("button2").onclick = showNum;
+    document.getElementById("button3").onclick = showNum;
+    document.getElementById("button4").onclick = showNum;
+    document.getElementById("button5").onclick = showNum;
+    document.getElementById("button6").onclick = showNum;
+    document.getElementById("button7").onclick = showNum;
+    document.getElementById("button8").onclick = showNum;
+    
+}
+
+
+// Get user input for 2 numbers
+function getInput()
+{
+    number1 = parseFloat()
+    numebr2 = parseFloat()
+
+}
