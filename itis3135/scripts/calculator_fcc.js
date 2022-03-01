64eb0b649f49324e9cac5a23c2b845c3d5d4a2b7
Will Nguyen <qnguye33@uncc.edu>
2022-02-28 23:16:45 -0500

    updated

---
+++ itis3135/scripts/calculator_fcc.js
@@ -0,0 +1,148 @@
+window.onload = function()
+{
+    const calculator = document.querySelector('.calculator')
+    const keys = calculator.querySelector('.calculator__keys')
+    const display = calculator.querySelector('.calculator__display')
+
+
+    keys.addEventListener('click', function(e)
+    {
+        if(e.target.matches('button'))
+        {
+            const key = e.target
+            const action = key.dataset.action
+            const keyContent = key.textContent
+            const displayedNum = display.textContent
+            const previousKeyType = calculator.dataset.previousKeyType
+
+            // Remove .is-depressed class from all keys
+            Array.from(key.parentNode.children)
+                .forEach(k => k.classList.remove('is-depressed'))
+
+
+            if(!action)
+            {
+                if(displayedNum === '0' || previousKeyType === 'operator' || previousKeyType === 'calculate')
+                {
+                    display.textContent = keyContent
+                }
+                else
+                {
+                    display.textContent = displayedNum + keyContent
+                }
+                calculator.dataset.previousKeyType = 'number'
+            }
+
+            if(action === 'decimal')
+            {
+                if(!displayedNum.includes('.'))
+                {
+                    display.textContent = displayedNum + '.'
+                }
+                else if (previousKeyType === 'operator' || previousKeyType === 'calculate')
+                {
+                    display.textContent = '0.'
+                }
+                
+                calculator.dataset.previousKeyType = 'decimal'
+            }
+            if(action === 'add' || action === 'subtract' || action === 'multiply' || action === 'divide')
+            {
+                
+                const firstValue = calculator.dataset.firstValue
+                const operator = calculator.dataset.operator
+                const secondValue = displayedNum
+                if(firstValue && operator && previousKeyType !== 'operator' && previousKeyType !== 'calculate')
+                {
+                    const calcValue = calculate(firstValue, operator, secondValue)
+                    display.textContent = calcValue
+                    calculator.dataset.firstValue = calcValue
+                }
+                else
+                {
+                    calculator.dataset.firstValue = displayedNum
+                }
+
+                key.classList.add('is-depressed')
+                    // Add custom attribute
+                    calculator.dataset.previousKeyType = 'operator'
+                    calculator.dataset.operator = action
+                
+            
+            }
+            
+            if(action === 'clear')
+            {
+                if(key.textContent === 'AC')
+                {
+                    calculator.dataset.firstValue = ''
+                    calculator.dataset.operator = ''
+                    calculator.dataset.previousKeyType = ''
+                }
+                else
+                {
+                key.textContent = 'AC'
+                }
+                display.textContent = 0
+                    calculator.dataset.previousKeyType = 'clear'
+            }
+
+
+            if(action !== 'clear')
+            {
+                const clearButton = calculator.querySelector('[data-action=clear]')
+                clearButton.textContent = 'CE'
+            }
+
+            if(action === 'calculate')
+            {
+                const firstValue = calculator.dataset.firstValue
+                const operator = calculator.dataset.operator
+                const secondValue = displayedNum
+                const modValue = calculator.dataset.modValue
+
+                if(firstValue)
+                {
+                    if(previousKeyType === 'calculate')
+                    {
+                        firstValue = displayedNum
+                    }
+                    
+                    display.textContent = calculate(firstValue, operator, secondValue)
+                }
+
+                //calculator.dataset.previousKeyType = 'calculate'
+                modValue = secondValue
+                calculator.dataset.previousKeyType = 'calculate'
+               
+            }
+        }
+
+    })
+    
+    const calculate = (n1, operator, n2) => {
+        let result = ''
+
+        if(operator === 'add')
+        {
+            result = parseFloat(n1) + parseFloat(n2)
+        }
+        if (operator === 'subtract')
+        {
+            result = parseFloat(n1) - parseFloat(n2)
+        }
+        if (operator === 'multiply')
+        {
+            result = parseFloat(n1) * parseFloat(n2)
+        }
+        if (operator === 'divide')
+        {
+            result = parseFloat(n1) / parseFloat(n2)
+        }
+
+        return result
+    }
+
+
+
+}
--- itis3135/scripts/calculator_try.js
+++ itis3135/scripts/calculator_try.js
@@ -1,25 +1,71 @@
-// Declare variable for calculator
-var number1;
-var number2;
 
-window.onload = function()
+// Function display values
+function display(value)
 {
-    document.getElementById("button1").onclick = showNum;
-    document.getElementById("button2").onclick = showNum;
-    document.getElementById("button3").onclick = showNum;
-    document.getElementById("button4").onclick = showNum;
-    document.getElementById("button5").onclick = showNum;
-    document.getElementById("button6").onclick = showNum;
-    document.getElementById("button7").onclick = showNum;
-    document.getElementById("button8").onclick = showNum;
-    
+    document.getElementById("total").value += value;
+}
+
+// Function evaluates the expression and calculate total
+function calculate()
+{
+    var cal = document.getElementById("total").value;
+    var cal1 = eval(cal);
+    document.getElementById("total").value = cal1;
+}
+
+// Function calculate squared
+function getSquared()
+{
+    var square = document.getElementById("total").value;
+    var square1 = Math.pow(square,2);
+    document.getElementById("total").value = square1;
+
+}
+
+// Function calculate Sin
+function getSin()
+{
+    var sin = document.getElementById("total").value;
+    var sin1 = Math.sin(sin);
+    document.getElementById("total").value = sin1;
+}
+
+// Funciton calculate Cosin
+function getCos()
+{
+    var cos = document.getElementById("total").value;
+    var cos1 = Math.cos(cos);
+    document.getElementById("total").value = cos1;
+}
+
+// Function calculate Tangent
+function getTan()
+{
+    var tan = document.getElementById("total").value;
+    var tan1 = Math.tan(tan);
+    document.getElementById("total").value = tan1;
+}
+
+// Function increment
+function getIncrement()
+{
+    var increment = document.getElementById("total").value;
+        increment++;
+        document.getElementById("total").value = increment;
+}
+
+// Function decrement
+function getDecrement()
+{
+    var decrement = document.getElementById("total").value;
+        decrement--;
+        document.getElementById("total").value = decrement;
 }
 
 
-// Get user input for 2 numbers
-function getInput()
+// Function clear all the values
+function clearScreen()
 {
-    number1 = parseFloat()
-    numebr2 = parseFloat()
-
+    document.getElementById("total").value = "";
 }
+
