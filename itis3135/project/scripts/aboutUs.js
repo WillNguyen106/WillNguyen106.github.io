33044544201e848fbfb1aaf8893cc3a84b35ff37
Will Nguyen <qnguye33@uncc.edu>
2022-04-26 18:39:54 -0400

    updated

---
+++ itis3135/project/scripts/aboutUs.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $("#slider").bxSlider({
+        auto: true,
+        minSlides: 1,
+        maxSlides: 1,
+        slideWidth: 500,
+        slideMargin: 20,
+        captions: true,
+        randomStart: true,
+        pause: 3000,
+        moveSlides:1,
+    
+    
+    });
+
+});
